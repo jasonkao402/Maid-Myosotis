517f4645dc79eaf1b0c2e97a113249e5eb43eef5
Jason_Not_Json <48216420+jasonkao402@users.noreply.github.com>
2021-08-17 21:43:48 +0800

    roleDict

--- cogs/reactionRole.py
+++ cogs/reactionRole.py
@@ -2,8 +2,10 @@ import discord
 from discord.ext import commands
 
 from extension.cog import CogExtension
+from tools import message
 
-KEYWORD = "**[KEYWORD]**找我領取身分組"
+KEYWORD = "[直接不用粗體不是更快嗎] 找我領取身分組"
+roleDict = {"❔": "A", "⚡": "B", "<:nsysu_isc:877159351272493058>": "資安社", "<:nsysu_cc:877159351582871552>": "程式研習社" }
 
 class React(CogExtension):
     __slots__ = ('bot')
@@ -12,21 +14,39 @@ class React(CogExtension):
         super().__init__(*args, **kwargs)
 
     @commands.command(name = 'lrole')
-    @commands.has_role('botMaster')
+    @commands.has_any_role('botMaster', '社團幹部')
     async def _listRole(self, ctx):
         print(", ".join([str(r.id) for r in ctx.guild.roles]))
         print(", ".join([str(r) for r in ctx.guild.roles]))
+
+    @commands.command(name = 'reactionRole')
+    @commands.has_any_role('botMaster', '社團幹部')
+    async def _reactionRole(self, ctx):
+        await ctx.message.delete()
+        msg = await ctx.send(message.codeblock(KEYWORD))
+        for k in roleDict:
+            try   : await msg.add_reaction(k)
+            except: pass
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user):
         if KEYWORD not in reaction.message.content:
             return
-        if reaction.emoji == "❔":
-            Role = discord.utils.get(user.guild.roles, name="A")
-            await user.add_roles(Role)
-        elif reaction.emoji == "⚡":
-            Role = discord.utils.get(user.guild.roles, name="B")
-            await user.add_roles(Role)
+        for k, v in roleDict.items():
+            if reaction.emoji == k :
+                Role = discord.utils.get(user.guild.roles, name=v)
+                if Role :
+                    await user.add_roles(Role)
+    
+    @commands.Cog.listener()
+    async def on_reaction_remove(self, reaction, user):
+        if KEYWORD not in reaction.message.content:
+            return
+        for k, v in roleDict.items():
+            if reaction.emoji == k :
+                Role = discord.utils.get(user.guild.roles, name=v)
+                if Role :
+                    await user.remove_roles(Role)
 
 
 def setup(bot):
